3c2ec7840b7b5c6f8644dd76c53660e116cdac65
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-02-13 17:22:06 +0000

    Added 2

---
+++ 2/README.md
@@ -0,0 +1,27 @@
+# Fibonacci #
+
+### Compliation ###
+`make`
+
+### Usage ###
+`./fibonacci <n>`
+
+### Test Data ###
+`./fibonacci 1`:  
+>The 1th number: 1 (iterative)  
+>The 1th number: 1 (recursive)
+
+`./fibonacci 10`:
+>The 10th number: 55 (iterative)  
+>The 10th number: 55 (recursive)
+
+
+`./fibonacci 44`:
+>The 10th number: 12586269025 (iterative)  
+>The 10th number: 12586269025 (recursive)
+
+Recursive method is very slow at big numbers
+
+
+### Cleaning ###
+`make clean`
---
+++ 2/fibonacci.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// Fibonacci function
+unsigned long long int fib_r(unsigned long long int n){
+    if(n < 3){
+        return 1;
+    } else {
+        return fib_r( n-1 ) + fib_r( n-2 );
+    }
+}
+
+// Iterative
+unsigned long long int fib_i(int n){
+    unsigned long long int nums[n - 1];
+
+    // Prime the nums array
+    nums[0] = 1;
+    nums[1] = 1;
+
+    for(int i = 2; i < n; i++){
+        nums[i] = nums[i-1] + nums[i-2];
+    }
+
+    return nums[n - 1];
+}
+
+int main(int argc, char *argv[]){
+    unsigned long long int n;
+    // Declare variables
+    if(argc != 2){
+        cout << "Usage: " << argv[0] << " <n>" << endl;
+        return 1;
+    } else {
+        n = atoi(argv[1]);
+    }
+
+    // Print out the results
+    cout << "The " << n << "th number: " << fib_i(n) << " (iterative)" << endl;
+    cout << "The " << n << "th number: " << fib_r(n) << " (recursive)" << endl;
+
+    return 0;
+}
