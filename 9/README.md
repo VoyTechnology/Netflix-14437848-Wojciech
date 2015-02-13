a2a6aa214204c9c4a51dd4e5b4ac893027bc883d
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-02-13 17:25:22 +0000

    Added 9

---
+++ 9/README.md
@@ -0,0 +1,22 @@
+# Sorting #
+
+### Compliation ###
+`make`
+
+### Usage ###
+`./sort <input_file>`
+
+### Test Data ###
+Sample input is included in input.txt
+```
+5 7 3 1
+```
+
+`./sort input.txt`:  
+```
+1 3 5 7
+```
+
+
+### Cleaning ###
+`make clean`
---
+++ 9/sort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string inputFile, outputFile;
+    if(argc != 3){
+        cout << "Usage: " << argv[0] << " <input> <output>" << endl;
+        return 1;
+    } else {
+        inputFile = argv[1];
+        outputFile = argv[2];
+    }
+
+	// Storage for array
+    vector <int> v;
+
+    ifstream input_file;
+    input_file.open(inputFile);
+
+    ofstream output_file;
+    output_file.open(outputFile);
+
+    int input;
+
+    while(input_file >> input){
+		v.push_back(input);
+    }
+
+    // Convert vector into an array
+    int a[v.size()];
+    std::copy(v.begin(), v.end(), a);
+
+    for(unsigned int i = 1; i < v.size(); i++){
+        int t1, t2;
+
+		while( a[i-1] < a[i]){
+			t1 = a[i - 1];
+			t2 = a[i];
+
+			a[i - 1] = t2;
+			a[i] = t1;
+			i--;
+		}
+    }
+
+	for(int i = (v.size() - 1); i > 0; i--){
+		output_file << a[i] << " ";
+	}
+
+	input_file.close();
+	output_file.close();
+
+    return 0;
+}
